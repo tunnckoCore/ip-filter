6cd0e78c0385f7b94348254f381448910a642172
tunnckoCore <mameto_100@mail.bg>
2016-10-06 15:48:39 +0300

    fix #3, make 3rd argument options object

--- index.js
+++ index.js
@@ -21,31 +21,55 @@ var toPath = require('to-file-path')
  *
  * console.log(ipFilter('123.77.34.89', '123.??.34.8*')) // => '123.77.34.89'
  * console.log(ipFilter('123.222.34.88', '123.??.34.8*')) // => null
- * console.log(ipFilter('123.222.34.88', ['123.*.34.*', '!123.222.*'])) // => null
  * console.log(ipFilter('123.222.33.1', ['123.*.34.*', '*.222.33.*'])) // => '123.222.33.1'
  *
- * // no strict mode
- * console.log(ipFilter('x-koaip', ['*-koaip', '!foo-koa*'], true)) // => 'x-koaip'
- * console.log(ipFilter('x-koa.foo', ['*-koa.*', '!foo-koa.*'], true)) // => 'x-koa.foo'
+ * // should notice the difference
+ * console.log(ipFilter('123.222.34.88', ['123.*.34.*', '!123.222.**']))
+ * // => null
+ * console.log(ipFilter('123.222.34.88', ['123.*.34.*', '!123.222.*']))
+ * // => '123.222.34.88'
+ *
+ * //
+ * // NON-STRICT mode
+ * //
+ *
+ * var res = ipFilter('x-koaip', ['*-koaip', '!foo-koa*'], { strict: false })
+ * console.log(res) // => 'x-koaip'
+ *
+ * var res = ipFilter('x-koa.foo', ['*-koa.*', '!foo-koa.*'], { strict: false })
+ * console.log(res) // => 'x-koa.foo'
  * ```
  *
- * @param  {String} `ip`
+ * @param  {String} `ip` Accepts only valid IPs by default
  * @param  {String|Array|RegExp|Function|Object} `patterns` Basically everything that [is-match][] can accept.
- * @param  {Boolean} `noStrict` Pass `true` if want to validate non-ip values.
+ * @param  {Object} `options` Pass `strict: false` if want to validate non-ip values,
+ *                            also passed to [is-match][] and so to [micromatch][]
  * @return {String|null} If not match returns `null`, otherwise the passed `ip`.
  * @api public
  */
 
-module.exports = function ipFilter (ip, patterns, noStrict) {
-  noStrict = typeof noStrict === 'boolean' ? noStrict : false
-
-  if (!noStrict && !ipRegex().test(ip)) {
-    throw new Error('ip-filter expect only valid IPv4/IPv6 IPs')
+module.exports = function ipFilter (ip, patterns, options) {
+  if (typeof ip !== 'string') {
+    throw new TypeError('ip-filter: expect `ip` to be a string')
   }
 
-  var match = noStrict
-    ? isMatch(patterns)(ip)
-    : isMatch(toPath(patterns))(toPath(ip))
+  options = typeof options === 'object' ? options : {}
+  options.strict = typeof options.strict === 'boolean' ? options.strict : true
 
-  return match ? ip : null
+  if (options.strict && !ipRegex().test(ip)) {
+    throw new Error('ip-filter: expect only valid IPs when `opts.strict` mode')
+  }
+
+  var id = options.strict ? tofp(ip) : ip
+  patterns = options.strict ? tofp(patterns) : patterns
+
+  return isMatch(patterns, options)(id) ? ip : null
+}
+
+function tofp (val) {
+  val = typeof val === 'string' ? toPath(val) : val
+  val = Array.isArray(val) ? val.map(function (p) {
+    return toPath(p)
+  }) : val
+  return val
 }
--- test.js
+++ test.js
@@ -10,21 +10,41 @@
 var test = require('assertit')
 var ipFilter = require('./index')
 
+test('should throw TypeError if `ip` not a string', function (done) {
+  function fixture () {
+    ipFilter(123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect `ip` to be a string/)
+  done()
+})
+
 test('should throw Error if not valid IPv4 or IPv6 ip is given', function (done) {
   function fixture () {
     ipFilter('foo.bar.baz.123', './**/.glob*')
   }
-
   test.throws(fixture, Error)
-  test.throws(fixture, /ip-filter expect only valid IPv4/)
+  test.throws(fixture, /expect only valid IPs when `opts.strict` mode/)
   done()
 })
 
-test('should return IP if match given patterns', function (done) {
-  var actual = ipFilter('123.77.34.89', '123.??.34.8*')
-  var expected = '123.77.34.89'
+test('should return IP if match string glob pattern', function (done) {
+  var actual1 = ipFilter('123.222.34.88', '123.??.34.8*') // => null
+  var actual2 = ipFilter('123.77.34.89', '123.??.34.8*') // => '123.77.34.89'
 
-  test.equal(actual, expected)
+  test.strictEqual(actual1, null)
+  test.strictEqual(actual2, '123.77.34.89')
+  done()
+})
+
+test('should return IP if match one of array glob patterns', function (done) {
+  var actual1 = ipFilter('123.222.34.88', ['123.*.34.*', '!123.222.**']) // => null
+  var actual2 = ipFilter('123.222.34.88', ['123.*.34.*', '!123.222.*']) // => '123.222.34.88'
+  var actual3 = ipFilter('123.222.33.1', ['123.*.34.*', '*.222.33.*']) // => '123.222.33.1'
+
+  test.strictEqual(actual1, null)
+  test.strictEqual(actual2, '123.222.34.88')
+  test.strictEqual(actual3, '123.222.33.1')
   done()
 })
 
@@ -32,21 +52,22 @@ test('should return null if not match', function (done) {
   var actual = ipFilter('123.222.34.89', ['123.???.34.8*', '!123.222.34.89'])
   var expected = null
 
-  test.equal(actual, expected)
+  test.strictEqual(actual, expected)
   done()
 })
 
 test('should support no strict mode to compare other than IPs', function (done) {
-  var actual = ipFilter('x-koaip', ['*-koaip', '!foo-koa'], true)
+  var actual1 = ipFilter('x-koaip', ['*-koaip', '!foo-koa'], { strict: false })
+  var actual2 = ipFilter('foo.bar.baz.123', 'foo.bar.**', { strict: false })
   var expected = 'x-koaip'
 
-  test.equal(actual, expected)
+  test.strictEqual(actual1, expected)
+  test.strictEqual(actual2, 'foo.bar.baz.123')
   done()
 })
 
-test('should work in no strict mode', function (done) {
-  var actual = ipFilter('x-koaip.foo', ['*-koaip.*', '!foo-koa.*'], true)
+test('should work in non strict mode', function (done) {
+  var actual = ipFilter('x-koaip.foo', ['*-koaip.*', '!foo-koa.*'], { strict: false })
   test.strictEqual(actual, 'x-koaip.foo')
   done()
 })
-
